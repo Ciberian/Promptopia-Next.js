82da6bd1e0d0ecd6e5b84b865ed362cec2081d0a
Ciberian <isi04047891@gmail.com>
2023-06-19 06:54:53 +0500

    Создаёт api-роут и всё необходимую логику, для аутентификации пользователя

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,7 +6,7 @@ import { useState, useEffect } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
@@ -20,24 +20,30 @@ const Nav = () => {
   };
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
       setProviders(response);
     };
 
-    setProviders();
+    setUpProviders();
   }, []);
 
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
-        <Image className="object-contain" src="/assets/images/logo.svg" width={30} height={30} alt="Promptopia Logo" />
+        <Image 
+          className="object-contain" 
+          src="/assets/images/logo.svg" 
+          width={30} 
+          height={30} 
+          alt="Promptopia Logo" 
+        />
         <p className="logo_text">Promptopia</p>
       </Link>
 
       {/* Desktop Navigation */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link className="black_btn" href="/create-prompt">
               Create Post
@@ -46,7 +52,13 @@ const Nav = () => {
               Sign Out
             </button>
             <Link href="/profile">
-              <Image className="rounded-full" src="/assets/images/logo.svg" width={37} height={37} alt="profile" />
+              <Image
+                className="rounded-full" 
+                src={session?.user?.image} 
+                width={37} 
+                height={37} 
+                alt="profile" 
+              />
             </Link>
           </div>
         ) : (
@@ -63,9 +75,16 @@ const Nav = () => {
 
       {/* Mobile Navigation */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
-            <Image className="rounded-full" src="/assets/images/logo.svg" width={37} height={37} alt="profile" onClick={dropdownClickHandler} />
+            <Image
+              className="rounded-full" 
+              src={session?.user?.image} 
+              width={37} 
+              height={37} 
+              alt="profile"
+              onClick={dropdownClickHandler} 
+            />
 
             {toggleDropdown && (
               <div className="dropdown">
